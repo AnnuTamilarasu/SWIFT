bceabc2e91b9cf9ed20bff441123ba816dc8731d
AnnuTamilarasu <ananya.tamilarasu@gmail.com>
2026-02-19 18:02:59 -0500

    added more pages

--- JS/navbar.js
+++ JS/navbar.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
   if (navbarContainer) {
     navbarContainer.innerHTML = `
-      <nav class="nav">
+    <div class="navbar">
+      <div class="top-nav">
+        <a href = "staffportal.html"><button>Staff Portal</button></a>
+      </div>
+      <div class="nav">
         <a href = "index.html"><button>Home</button></a>
-        <a href = "help.html"><button>Help</button></a>
-        <a href = "help.html"><button>Help</button></a>
-        
-      </nav>`}
+        <a href = "admissions.html"><button>Admissions</button></a>
+        <a href = "teachers.html"><button>Staff</button></a>
+        <a href = "projects.html"><button>Projects</button></a>
+
+      </div>
+    </div>`
+  }
 });
